e29042c97b1fa4187b15bbd89434e0b726a61a27
Christos Mizikakis <cmizikakis@gmail.com>
2019-02-22 09:48:17 +0200

    Create Admin menu

---
+++ BooksRentApp/Scripts/AdminMenu.js
@@ -0,0 +1,9 @@
+﻿$(function () {
+    $('[data-admin-menu]').hover(function () {
+
+        $('[data-admin-menu]').toggleClass('open');
+    })
+
+});
+//1) we have to add this to our BundleConfig 
+//2) change the reference to _Layout.cshtml from Scripts.Render("~/bundles/bootstrap") to
