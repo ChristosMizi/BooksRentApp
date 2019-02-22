8f2dd6716c1e4338d96ac00378193ef69943e164
Christos Mizikakis <cmizikakis@gmail.com>
2019-02-22 14:27:50 +0200

    Enable migrations- Add BookAndGenre classes to the database

---
+++ BooksRentApp/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace BooksRentApp.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<BooksRentApp.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(BooksRentApp.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
