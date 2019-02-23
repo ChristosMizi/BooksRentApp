eb082056e04b96ac76e6ad0cfa1c32c2b5e04028
Christos Mizikakis <cmizikakis@gmail.com>
2019-02-23 15:50:50 +0200

    1) Create Genre Controller and View 2) Create Partial views _BackToLisTButtonPartial AND _CreateButtonPartial

---
+++ BooksRentApp/Controllers/GenreController.cs
@@ -0,0 +1,57 @@
+﻿using BooksRentApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BooksRentApp.Controllers
+{
+    public class GenreController : Controller
+    {
+        //1. we should create a DBContext Appliation obj, that is the obj through which we will be conecting to the db
+        //and accessing the Genre table
+
+
+        private ApplicationDbContext db;
+
+        // initialize the obj
+
+        public GenreController()
+        {
+            db = new ApplicationDbContext();
+        }
+        // GET: Genre
+        public ActionResult Index()
+        {
+            return View(db.Genres.ToList());
+        }
+        //it's just creating a new Genre, so i don't have to pass anything to the view
+        //then i create the view for this action
+        //This is a Get Action
+        public ActionResult Create()
+        {
+
+            return View();
+        }
+
+        //Post Action
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Genre genre)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Genres.Add(genre);
+                db.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+
+        }
+    }
+}
